80be060e1b60558c25bff8c846db40f58b814380
David Fowler <davidfowl@gmail.com>
2020-04-20 15:16:17 -0700

    WIP

---
+++ LegacyApplication/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="LegacyApplication.MvcApplication" Language="C#" %>
---
+++ LegacyApplication/Global.asax.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Web;
+using System.Web.Compilation;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+
+namespace LegacyApplication
+{
+    public class MvcApplication : System.Web.HttpApplication
+    {
+        protected void Application_Start()
+        {
+            AreaRegistration.RegisterAllAreas();
+            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+        }
+
+        private void Application_PostResolveRequestCache(object sender, EventArgs e)
+        {
+            var controllerName = (string)Context.Request.RequestContext.RouteData.Values["controller"];
+
+            if (!string.IsNullOrEmpty(controllerName))
+            {
+                var factory = ControllerBuilder.Current.GetControllerFactory();
+                IController controller = null;
+                try
+                {
+                    controller = factory.CreateController(Context.Request.RequestContext, controllerName);
+                }
+                catch
+                {
+                    // TODO: Use descriptors instead but it's all internal :(
+                }
+
+                // The controller doesn't exist, then remap the handler
+                if (controller == null)
+                {
+                    Context.RemapHandler(null);
+                }
+                else
+                {
+                    // REVIEW: Figure out if there's an action matching but all of that stuff is hard to get at
+                    // var actionName = (string)Context.Request.RequestContext.RouteData.Values["action"];
+
+                    // TODO: Reuse the controller instance with a custom factory (if it hasn't been overridden)
+
+                    // Release the controller
+                    factory.ReleaseController(controller);
+                }
+            }
+        }
+    }
+}
