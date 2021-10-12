dad65f82ace6232f57eba92294d2e4a6d0f88d14
Ken Schlobohm <keschlob@microsoft.com>
2021-10-12 17:03:08 -0500

    adds mvc music store with IIS crossover for weather api

---
+++ MvcMusicStore.Core/Controllers/WeatherController.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+
+namespace MvcMusicStore.Core.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class WeatherController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly ILogger<WeatherController> _logger;
+
+        public WeatherController(ILogger<WeatherController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IEnumerable<WeatherForecast> Get()
+        {
+            var rng = new Random();
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = rng.Next(-20, 55),
+                Summary = Summaries[rng.Next(Summaries.Length)]
+            })
+            .ToArray();
+        }
+
+        [HttpGet("pid")]
+        public int GetPid()
+        {
+            return Process.GetCurrentProcess().Id;
+        }
+    }
+}
---
+++ MvcMusicStore.Legacy/Global.asax.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace MvcMusicStore
+{
+    // Note: For instructions on enabling IIS6 or IIS7 classic mode, 
+    // visit http://go.microsoft.com/?LinkId=9394801
+
+    public class MvcApplication : System.Web.HttpApplication
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+        private void Application_PostResolveRequestCache(object sender, EventArgs e)
+        {
+            // Here we're going to decide which routes map to ASP.NET Core and which ones map to ASP.NET 
+            // - /api/weather/ to ASP.NET Core
+            // - /api/values to ASP.NET
+
+            // At this point we've set the handler to take this request, now if this route maps to one
+            // we want to redirect to ASP.NET Core, so set the handler to null
+            if (Context.Request.Path.StartsWith("/api/weather", StringComparison.OrdinalIgnoreCase) ||
+                Context.Request.Path.StartsWith("/css", StringComparison.OrdinalIgnoreCase))
+            {
+                // Set a server variable pass state to the ASP.NET Core module
+                Context.Request.ServerVariables["FromFramework"] = "ASP.NET";
+
+                Context.RemapHandler(null);
+            }
+        }
+
+        public static void RegisterRoutes(RouteCollection routes)
+        {
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+
+            routes.MapRoute(
+                "Default", // Route name
+                "{controller}/{action}/{id}", // URL with parameters
+                new { controller = "Home", action = "Index", id = UrlParameter.Optional } // Parameter defaults
+            );
+
+        }
+
+        protected void Application_Start()
+        {
+            System.Data.Entity.Database.SetInitializer(new MvcMusicStore.Models.SampleData());
+
+            AreaRegistration.RegisterAllAreas();
+
+            RegisterGlobalFilters(GlobalFilters.Filters);
+            RegisterRoutes(RouteTable.Routes);
+        }
+    }
+}
