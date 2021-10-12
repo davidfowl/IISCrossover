eb7778bb50bf46aaf3bfaa587c45937b9541758e
Ken Schlobohm <keschlob@microsoft.com>
2021-10-12 14:02:34 -0500

    adds eShopModernizing with IIS cross over

---
+++ eShopLegacyMVC/App_Start/RouteConfig.cs
@@ -0,0 +1,20 @@
+﻿using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace eShopLegacyMVC
+{
+    public class RouteConfig
+    {
+        public static void RegisterRoutes(RouteCollection routes)
+        {
+            routes.MapMvcAttributeRoutes();
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+
+            routes.MapRoute(
+                name: "Default",
+                url: "{controller}/{action}/{id}",
+                defaults: new { controller = "Catalog", action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
---
+++ eShopLegacyMVC/Global.asax.cs
@@ -0,0 +1,134 @@
+﻿using Autofac;
+using Autofac.Integration.Mvc;
+using Autofac.Integration.WebApi;
+using eShopLegacyMVC.Models;
+using eShopLegacyMVC.Models.Infrastructure;
+using eShopLegacyMVC.Modules;
+using log4net;
+using System;
+using System.Configuration;
+using System.Data.Entity;
+using System.Diagnostics;
+using System.Reflection;
+using System.Web;
+using System.Web.Http;
+using System.Web.Mvc;
+using System.Web.Optimization;
+using System.Web.Routing;
+
+namespace eShopLegacyMVC
+{
+    public class MvcApplication : HttpApplication
+    {
+        private static readonly ILog _log = LogManager.GetLogger(MethodBase.GetCurrentMethod().DeclaringType);
+
+        IContainer container;
+
+
+        private void Application_PostResolveRequestCache(object sender, EventArgs e)
+        {
+            // Here we're going to decide which routes map to ASP.NET Core and which ones map to ASP.NET 
+            // - /api/weather/ to ASP.NET Core
+            // - /api/values to ASP.NET
+
+            // At this point we've set the handler to take this request, now if this route maps to one
+            // we want to redirect to ASP.NET Core, so set the handler to null
+            if (Context.Request.Path.StartsWith("/api/weather", StringComparison.OrdinalIgnoreCase) ||
+                Context.Request.Path.StartsWith("/WebForm1.aspx", StringComparison.OrdinalIgnoreCase))
+            {
+                // Set a server variable pass state to the ASP.NET Core module
+                Context.Request.ServerVariables["FromFramework"] = "ASP.NET";
+
+                Context.RemapHandler(null);
+            }
+        }
+
+        protected void Application_Start()
+        {
+            container = RegisterContainer();
+            GlobalConfiguration.Configure(WebApiConfig.Register);
+            AreaRegistration.RegisterAllAreas();
+            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
+            ConfigDataBase();
+        }
+
+        /// <summary>
+        /// Track the machine name and the start time for the session inside the current session
+        /// </summary>
+        protected void Session_Start(Object sender, EventArgs e)
+        {
+            HttpContext.Current.Session["MachineName"] = Environment.MachineName;
+            HttpContext.Current.Session["SessionStartTime"] = DateTime.Now;
+        }
+
+        protected void Application_BeginRequest(object sender, EventArgs e)
+        {
+            //set the property to our new object
+            LogicalThreadContext.Properties["activityid"] = new ActivityIdHelper();
+
+            LogicalThreadContext.Properties["requestinfo"] = new WebRequestInfo();
+
+            _log.Debug("WebApplication_BeginRequest");
+        }
+
+        /// <summary>
+        /// http://docs.autofac.org/en/latest/integration/mvc.html
+        /// </summary>
+        protected IContainer RegisterContainer()
+        {
+            var builder = new ContainerBuilder();
+
+            var thisAssembly = Assembly.GetExecutingAssembly();
+            builder.RegisterControllers(thisAssembly);
+            builder.RegisterApiControllers(thisAssembly);
+
+            var mockData = bool.Parse(ConfigurationManager.AppSettings["UseMockData"]);
+            builder.RegisterModule(new ApplicationModule(mockData));
+
+            var container = builder.Build();
+
+            // set mvc resolver
+            DependencyResolver.SetResolver(new AutofacDependencyResolver(container));
+
+            // set webapi resolver
+            var resolver = new AutofacWebApiDependencyResolver(container);
+            GlobalConfiguration.Configuration.DependencyResolver = resolver;
+
+            return container;
+        }
+
+        private void ConfigDataBase()
+        {
+            var mockData = bool.Parse(ConfigurationManager.AppSettings["UseMockData"]);
+
+            if (!mockData)
+            {
+                Database.SetInitializer<CatalogDBContext>(container.Resolve<CatalogDBInitializer>());
+            }
+        }
+
+    }
+
+    public class ActivityIdHelper
+    {
+        public override string ToString()
+        {
+            if (Trace.CorrelationManager.ActivityId == Guid.Empty)
+            {
+                Trace.CorrelationManager.ActivityId = Guid.NewGuid();
+            }
+
+            return Trace.CorrelationManager.ActivityId.ToString();
+        }
+    }
+
+    public class WebRequestInfo
+    {
+        public override string ToString()
+        {
+            return HttpContext.Current?.Request?.RawUrl + ", " + HttpContext.Current?.Request?.UserAgent;
+        }
+    }
+}
