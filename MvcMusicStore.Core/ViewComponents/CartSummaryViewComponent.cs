beb4b51fef3b6991aa48a1da479d8a14c39a437d
Ken Schlobohm <keschlob@microsoft.com>
2021-10-15 10:34:28 -0500

    created view components for partials

---
+++ MvcMusicStore.Core/ViewComponents/CartSummaryViewComponent.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MvcMusicStore.Models;
+
+namespace MvcMusicStore.Core.ViewComponents
+{
+    public class CartSummaryViewComponent : ViewComponent
+    {
+        private readonly IHttpContext _httpContext;
+
+        public CartSummaryViewComponent(IHttpContext httpContext)
+        {
+            _httpContext = httpContext;
+        }
+
+        public IViewComponentResult Invoke(int numberOfItems)
+        {
+            var cart = ShoppingCart.GetCart(_httpContext);
+
+            ViewData["CartCount"] = cart.GetCount();
+            return View();
+        }
+    }
+}
